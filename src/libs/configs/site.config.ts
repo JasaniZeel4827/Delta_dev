453d3269fa276de4dc9d83a5d1d35d6217dfde8c
Zeel <deltapixel8a@gmail.com>
2025-07-02 21:09:51 +0530

    deltadev

---
+++ src/libs/configs/site.config.ts
@@ -0,0 +1,26 @@
+export const siteConfig = {
+  profile: {
+    name: 'Jasani Zeel',
+    title: 'Software Engineer',
+    avatar: '/assets/zeelpro.png',
+    location: 'Junagadh, India',
+    experience: 'B.Tech Computer Science',
+    about:
+      'I am a Computer Science student from India with an interest in full stack webapp, mobile app, system design, and Generative AI.',
+    keywords: ['Software Engineer', 'Full Stack Developer', 'React', 'Node.js', 'python'],
+  },
+  contact: {
+    email: 'jasanizeel487@gmail.com',
+    phone: '+1234567890',
+    emailHref: 'mailto:jasanizeel487@gmail.com',
+    phoneHref: 'tel:+1234567890',
+  },
+  social: {
+    githubHandle: 'JasaniZeel4827',
+    githubHref: 'https://github.com/JasaniZeel4827',
+    twitterHandle: 'Jasani  Zeel',
+    twitterHref: 'https://x.com/deltapixel3777',
+    linkedinHandle: '',
+    linkedinHref: '#',
+  },
+}
